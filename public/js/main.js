e36f3648da1042de1b432af64fb87a685b654228
Frankie <franjmnez9@gmail.com>
2023-04-13 19:54:54 -0500

    fixed connection to db

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
-const nextFact = document.querySelector('button')
-nextFact.addEventListener('click', getNextFact)
-let factNum = 0
+// const nextFact = document.querySelector('button')
+// nextFact.addEventListener('click', getNextFact)
+// let factNum = 0
 
-async function getNextFact() {
-    factNum++
-    const response = await fetch('/fact')
-    const data = await response.json()
-    if (factNum >= data.length) factNum = 0;
-    document.querySelector('p').innerText = data[factNum].fact
-}
+// async function getNextFact() {
+//     factNum++
+//     const response = await fetch('/fact')
+//     const data = await response.json()
+//     if (factNum >= data.length) factNum = 0;
+//     document.querySelector('p').innerText = data[factNum].fact
+// }
--- server.js
+++ server.js
@@ -4,38 +4,48 @@ const MongoClient = require('mongodb').MongoClient
 const PORT = 2121 //setting local port number
 require('dotenv').config() //setting private env files
 
-let db, //directing conection to database to env file
+let db,
+    quotesCollection, //directing conection to database to env file
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'catFact' //db name
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //connecting to database and getting comfirmation.
+    MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //connecting to database and getting comfirmation.
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
+        quotesCollection = db.collection('facts')
     })
+        // Move route handlers and any code that needs to access `db` inside this block
+        app.set('view engine', 'ejs')
+        app.use(express.static('public'))
+        app.use(express.urlencoded({extended : true}))
+        app.use(express.json())
 
-    app.set('view engine', 'ejs') //setting view engine
-    app.use(express.static('public')) //accesing public folders
-    app.use(express.urlencoded({extended : true})) //setting body parser
-    app.use(express.json()) //getting access to json
+        app.get('/', async (req, res) => {
+            const catFacts = await db.collection('facts').find().toArray()
+            res.render('index.ejs', {fact: catFacts})
+            console.log(catFacts)
+        })
 
-    //main
-    app.get('/' , async (req, res) => {
-        const catFacts = await db.collection('facts').find().toArray()
-        res.render('index.ejs', {fact: catFacts})
-        console.log(catFacts)
-    })
+        app.get('/fact', async (req, res) => {
+            const catFacts = await db.collection('facts').find().toArray()
+            res.send(catFacts)
+            console.log(catFacts);
+        })
 
-    app.get('/fact' , async (req, res) => {
-        const catFacts = await db.collection('facts').find().toArray()
-        res.send(catFacts)
-        console.log(catFacts)
-    })
+        app.post('/newFact', (req, res) => {
+            quotesCollection.insertOne(req.body)
+              .then(result => {
+                console.log(result)
+                location.reload()
+              })
+              .catch(error => console.error(error))
+          })
 
-    app.post('/newFact', async (req, res)=> {
-        console.log('asdasd')
-    })
+        app.delete('/deleteFact', (req, res) => {
+            quotesCollection.deleteOne(req.body)
+        })
 
-    app.listen(PORT, () => {
-        console.log('SERVER UP')
-    })
+        app.listen(PORT, () => {
+            console.log('SERVER UP')
+        })
